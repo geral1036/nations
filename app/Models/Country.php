c7a0481551cc7c8ac3b2cf95490b1364afd46461
Geral1027 <pgeraldine534@gmail.com>
2022-07-27 10:09:27 -0500

    proyecto inicializado

---
+++ app/Models/Country.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    //la tabla a conectar
+    protected $table ="countries";
+
+    //la clave primaria de esa tabla
+    protected $primaryKey="country_id";
+
+    //omitir campos de auditoria
+    public $timestamp=false;
+    use HasFactory;
+}
